901018366627308138d60bdedc8af08cf17d3ec9
Scott Holland <scotthollandjr@gmail.com>
2016-04-19 13:14:55 -0700

    First test pass

---
+++ src/main/java/NumbersToWords.java
@@ -0,0 +1,9 @@
+public class NumbersToWords {
+  public static void main(String[] args) {
+
+  }
+
+  public static String turnNumbersToWords(Integer inputNumber) {
+    return "one";
+  }
+}
---
+++ src/test/java/NumbersToWordsTest.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class NumbersToWordsTest {
+
+  @Test
+  public void turnNumbersToWords_singleDigits_String() {
+    NumbersToWords turnNumbersToWords = new NumbersToWords();
+    String expected = "one";
+    assertEquals(expected, turnNumbersToWords.turnNumbersToWords(1));
+  }
+
+}
